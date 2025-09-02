682ae9780946976c4b40637d9a6e9e364fb493f1
Zoidbergggggg <dkovalenko314@gmail.com>
2025-09-02 23:03:07 +0300

    add singleton

--- FavoriteColors.cpp
+++ FavoriteColors.cpp
@@ -1,6 +1,93 @@
 #include <iostream>
 
-int color = 0;
+class ColorsSingleton
+{
+private:
+	int color = 0;
+
+	static ColorsSingleton* m_instance;
+	ColorsSingleton() = default;
+
+	ColorsSingleton(ColorsSingleton&) = delete;
+	ColorsSingleton& operator = (ColorsSingleton&) = delete;
+	ColorsSingleton(ColorsSingleton&&) = delete;
+	ColorsSingleton& operator = (ColorsSingleton&&) = delete;
+
+	int GetColorBit(int ColorIndex)
+	{
+		int i = 1;
+		i = i << ColorIndex;
+		return i;
+	}
+
+public:
+
+	static ColorsSingleton* instance()
+	{
+		if (m_instance == nullptr)
+		{
+			m_instance = new ColorsSingleton();
+		}
+		return m_instance;
+	}
+
+	void AddColor(const int ColorIndex)
+	{
+
+		if (ColorIndex <= 12 && ColorIndex >= 0)
+		{
+			int i = GetColorBit(ColorIndex);
+			color = color | i;
+			std::cout << "Successfully added:) \n\n";
+		}
+	}
+	void RemoveColor(const int RemoveColor)
+	{
+
+		if (RemoveColor <= 12 && RemoveColor >= 0)
+		{
+			int i = GetColorBit(RemoveColor);
+			if (color & i)
+			{
+				color = color ^ i;
+				std::cout << "Successfully removed :( \n\n";
+			}
+			else
+			{
+				std::cout << "This color wasn't your favorite \n\n";
+			}
+		}
+	}
+
+	void PrintFavoriteColors()
+	{
+
+		if (color == 0)
+		{
+			std::cout << "You have no favorite colors\n";
+		}
+		else
+		{
+			int ColorIndex = 0;
+			std::cout << "Your favorite colors are:";
+			while (ColorIndex < 13)
+			{
+				int i = GetColorBit(ColorIndex);
+				int Favorite = color & i;
+				if (Favorite)
+				{
+					std::cout << " " << ColorIndex;
+				}
+				ColorIndex++;
+			}
+			std::cout << "\n";
+		}
+
+
+	}
+};
+
+ColorsSingleton* ColorsSingleton::m_instance = nullptr;
 
 int GetColorIndex()
 {
@@ -14,12 +101,6 @@ int GetColorIndex()
 
 	std::cin >> ColorIndex;
 	return ColorIndex;
-}
-int GetColorBit(int ColorIndex)
-{
-	int i = 1;
-	i = i << ColorIndex;
-	return i;
 }
 
 
@@ -37,54 +118,22 @@ int main()
 
 		if (input == 1)
 		{
+			ColorsSingleton* s = ColorsSingleton::instance();
 			int ColorIndex = GetColorIndex();
-			if (ColorIndex <= 12 && ColorIndex >= 0)
-			{
-				int i = GetColorBit(ColorIndex);
-				color = color | i;
-				std::cout << "Successfully added:) \n\n";
-			}
+			s->AddColor(ColorIndex);
+
 		}
 		else if (input == 2)
 		{
-			int RemoveColor = GetColorIndex();
-			if (RemoveColor <= 12 && RemoveColor >= 0)
-			{
-				int i = GetColorBit(RemoveColor);
-				if (color & i)
-				{
-					color = color ^ i;
-					std::cout << "Successfully removed :( \n\n";
-				}
-				else 
-				{
-					std::cout << "This color wasn't your favorite \n\n";
-				}
-			}
-		}
-		else if(input == 3)
-		{ 
-			if (color == 0)
-			{
-				std::cout << "You have no favorite colors\n";
-			}
-			else
-			{
-				int ColorIndex = 0;
-				std::cout << "Your favorite colors are:";
-				while (ColorIndex < 13)
-				{
-					int i = GetColorBit( ColorIndex);
-					int Favorite = color & i;
-					if (Favorite)
-					{
-						std::cout << " " << ColorIndex;
-					}
-					ColorIndex++;
-				}
-				std::cout << "\n";
-			}
+			ColorsSingleton* s = ColorsSingleton::instance();
+			int ColorIndex = GetColorIndex();
+			s->RemoveColor(ColorIndex);
 
+		}
+		else if (input == 3)
+		{
+			ColorsSingleton* s = ColorsSingleton::instance();
+			s->PrintFavoriteColors();
 		}
 		else if (input == 4)
 		{
